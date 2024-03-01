742ba6a0ba9b41b1d5c8f9d9d131d835b6f410a0
MutheuMc <cheldeanmutheu1@gmail.com>
2024-03-01 23:11:41 +0300

    dist update

--- api/index.js
+++ api/index.js
@@ -19,14 +19,14 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = "bsbsfbrnsftentwnnwnwn";
 
 // Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, './dist')));
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
    cors({
       credentials: true,
-      origin: "http://localhost:5173",
+      
    })
 );
 
